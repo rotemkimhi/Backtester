796d5b640bc16df3756a74f0b59035360f821491
rotemkimhi <rotemkim@post.bgu.ac.il>
2025-06-24 19:37:06 +0300

    deployy changes

--- frontend/src/components/StrategyFileLoader.tsx
+++ frontend/src/components/StrategyFileLoader.tsx
@@ -25,12 +25,11 @@ export default function StrategyFileLoader({
   const [dropdownLoading, setDropdownLoading] = useState(false);
 
   async function fetchStrategies() {
+    console.log("VITE_API_URL:", import.meta.env.VITE_API_URL);
     setDropdownLoading(true);
     try {
       //from backend/src/routes/strategies.ts
-      console.log(import.meta.env.VITE_API_URL)
       const res = await axios.get(`${import.meta.env.VITE_API_URL}/backend/strategies`);
-      console.log(import.meta.env.VITE_API_URL)
       console.log("Fetched strategies:", res.data);
       setStrategies(Array.isArray(res.data) ? res.data : []);
     } catch (err) {
